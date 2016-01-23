a342d8f6700c47388c7292856cd5c6502f3f6385
Cheng <g.cheng14@gmail.com>
2016-01-23 17:07:54 -0500

    add expedia api call and parse the result, return as an array of locations

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ Clarifai.initAPI(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
 
 
 var stdio = require('stdio');
+var request = require("request")
+
 
 // support some command-line options
 var opts = stdio.getopt( {
@@ -185,10 +187,77 @@ function getUserUrls(user_token) {
 //   // Clarifai.setRequestTimeout( 100 ); // in ms - expect: force a timeout response
 //   // Clarifai.setRequestTimeout( 100 ); // in ms - expect: ensure no timeout
 
-//   Clarifai.tagURL( testImageURL , ourId, commonResultHandler );
-// }
+
 
 // exampleTagSingleURL();
 tagMultipleURL();
 // exampleFeedback();
 Clarifai.clearThrottleHandler();
+
+
+var sExpediaKey = "Rb4v7KFeKxU0bTOjqFg9kDTrzZgSQpWc";
+var sExpediaRequestRoot = "http://terminal2.expedia.com/x/nlp/results?q=";
+var sCompleteKey = "&apikey=" + sExpediaKey;
+
+var sQuery1 = "people"+ "landscape "+ "group "+ "daylight "+ "adult "+ "rock "+ "seashore "+ "travel "+ "environment "+ "recreation "+ "tourism "+ "man "+ "mountain "+ "outdoors "+ "many "+ "scenic " + "motion "+ "tourist "+ "sky "+ "adventure ";
+var sQuery2 = "beach "+  "water "+  "people "+  "sea "+  "seashore "+  "travel "+  "ocean "+  "leisure "+  "recreation "+  "sand "+  "group "+  "outdoors "+  "man "+  "adult "+  "daylight "+  "lifestyle "+  "vacation "+  "landscape "+  "sky "+  "summer ";
+var sQuery3 = "indoors "+"window "+"interior design "+"furniture "+"no person "+"room "+"contemporary "+"seat "+"house "+"chair "+"curtain "+"home "+"table "+"luxury "+"wood "+"easy chair "+"apartment "+"architecture "+"rug "+"family"
+var sQuery4 = "architecture winter snow outdoors travel church religion no person sky traditional building cross cold Christmas old tourism Orthodox landmark spirituality city";
+var sQuery5 = "vehicle people competition race group festival many group transportation system adult man track race rally auto racing championship action road woman hurry"
+
+var sFinalQuery = sExpediaRequestRoot + sQuery5 + sCompleteKey
+console.log(sFinalQuery);
+
+function printArray(array) {
+  array.forEach(function(element) {
+    console.log(element);
+  });
+}
+
+function getNames(aElement) {
+  var aName = aElement.map(function(aElement){
+    return aElement.name;
+  });
+  printArray(aName);
+}
+
+function parseExpediaResponse(oResponse) {
+  var oResponseResult = oResponse.result;
+  if (oResponseResult.pois !== undefined) {
+    // console.log(oResponseResult.pois);
+    getNames(oResponseResult.pois);
+  }
+  else if (oResponseResult.regions !== undefined) {
+    // console.log(oResponseResult.pois);
+    getNames(oResponseResult.regions);
+  }
+  else if (oResponseResult.clusters !== undefined) {
+    // console.log(oResponseResult.pois);
+  }
+  else {
+    console.log("Error");
+  }
+}
+
+request({
+   url: sFinalQuery,
+   json: true
+}, function (error, response, body) {
+   if (!error && response.statusCode === 200) {
+      // console.log(body.result.pois) // Print the json response
+      parseExpediaResponse(body);
+   }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
